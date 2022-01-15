8e4d811b21cb07105fe00af0e9e3cad65b6fc836
velopman <74576289+velopman@users.noreply.github.com>
2022-01-15 15:53:29 -0500

    add test kraken and shaders

--- source/main.gd
+++ source/main.gd
@@ -1,1 +1,18 @@
-extends Node2D
+extends ColorRect
+
+
+# Private constants
+
+const __center_screen: Vector2 = Vector2(640.0, 360.0)
+
+
+# Lifecycle methods
+
+func _ready() -> void:
+	material.set_shader_param("line_start", __center_screen)
+
+
+func _process(delta: float) -> void:
+	var position_current: Vector2 = get_viewport().get_mouse_position()
+	material.set_shader_param("line_end", position_current)
+
