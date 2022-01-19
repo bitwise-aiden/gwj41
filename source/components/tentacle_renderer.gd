59afdf2525fceadfca837cb688c84fc27ba8758c
velopman <74576289+velopman@users.noreply.github.com>
2022-01-19 00:19:01 -0500

    Make tentacle shader human readable

--- source/components/tentacle_renderer.gd
+++ source/components/tentacle_renderer.gd
@@ -11,7 +11,6 @@ var __vertices: Array = [Vector2(640.0, 360.0), Vector2.ZERO]
 
 # Lifecycle methods
 
-
 func _process(delta: float) -> void:
 	var tentacles_raw: Array = get_tree().get_nodes_in_group("tentacle")
 	var tentacles: Array = []
@@ -32,7 +31,7 @@ func _process(delta: float) -> void:
 					tentacle.rope_points,
 					30.0,
 					10.0,
-					3
+					4
 				)
 			)
 
@@ -64,6 +63,6 @@ func set_points(tentacles: Array) -> void:
 
 	__texture.create_from_image(__image)
 
-	material.set_shader_param("vertices_array", __texture)
-	material.set_shader_param("array_count", row_count)
+	material.set_shader_param("tentacles", __texture)
+	material.set_shader_param("tentacle_count", row_count)
 
