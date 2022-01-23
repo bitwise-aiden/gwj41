3a7866be0d2afc715818977daab45bd139d53362
Orbitoid <Orbitoid@users.noreply.github.com>
2022-01-23 22:14:58 +1100

    ships now break and stick around

--- source/scenes/main/main.gd
+++ source/scenes/main/main.gd
@@ -3,7 +3,7 @@ extends Node2D # Will this break things? \n\r\l """ - TheoremMetal
 var Rope = preload("res://Parts/Rope.tscn")
 var Ship = preload("res://Parts/Ship.tscn")
 var Parrot = preload("res://Parts/Parrot.tscn")
-
+var Broken_Ship = preload("res://Parts/Broken_Ship.tscn")
 var parrots = []
 var ships = []
 var start_pos := Vector2.ZERO
@@ -25,9 +25,8 @@ Vector2(Globals.initial_end_right_tentacle_position.x + 150,560)]
 onready var centerText = get_tree().get_root().get_node("main/text")
 var time_start = 0
 var time_now = 0
-
-func kill_ship(ship):
-	ships.remove(ships.find(ship))
+	# Spawn two broken halves of the ship
+	# spawn a ghost ship
 func kill_parrot(parrot):
 	parrots.remove(parrots.find(parrot))
 
@@ -37,19 +36,24 @@ func _ready():
 	Event.connect("emit_ship_death", self, "kill_ship")
 	Event.connect("emit_parrot_death", self, "kill_parrot")
 	Event.connect("spawn_parrot", self, "spawn_parrot")
+	Event.connect("spawn_new_broken_ship", self, "make_broken_ship")
 	left_tentacle = spawn_tentacle(Globals.initial_start_left_tentacle_position, Globals.initial_end_left_tentacle_position)
 	right_tentacle = spawn_tentacle(Globals.initial_start_right_tentacle_position, Globals.initial_end_right_tentacle_position)
-
 	Event.emit_signal("emit_audio", {"type": "music", "name": "background"})
-
 	spawn_ship(Vector2(Globals.projectResolution.x, 180))
 	time_start = OS.get_unix_time()
-
 	Transition.fade_in()
-
 	Globals.hugScore = Globals.hugScoreInitial
 
+func kill_ship(ship):
+	#print("ship to kill: ", ship, " at position: ", ship.global_position)
+	ships.remove(ships.find(ship))
 
+func make_broken_ship(ship):
+	var BS = Broken_Ship.instance()
+	add_child(BS)
+	BS.leftHalf.global_position = ropes[0].getRopeEndPoint()
+	BS.rightHalf.global_position = ropes[1].getRopeEndPoint()
 
 func spawn_tentacle(start_pos, end_pos):
 	if len(ropes) <= max_tentacles:
@@ -66,7 +70,7 @@ func spawn_ship(start_pos):
 	ships.append(ship)
 	ship.global_position = start_pos
 	add_child(ship)
-	
+
 func spawn_parrot():
 	var parrot = Parrot.instance()
 	parrots.append(parrot)
@@ -76,7 +80,6 @@ func spawn_parrot():
 func reset_decorative_tentacles_positions():
 	for i in range(len(ropes)):
 		ropes[i].setRopeEndPoint(decorative_tentacles_initial_positions[i])
-
 
 func _physics_process(delta):
 	if Globals.hugScore <= 0:
