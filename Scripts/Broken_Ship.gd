54b89328f64640a81055aca23ccd5db04588c8be
Orbitoid <Orbitoid@users.noreply.github.com>
2022-01-24 01:20:28 +1100

    changes the ship's z-index to -1 to make it look like it is underwater

--- Scripts/Broken_Ship.gd
+++ Scripts/Broken_Ship.gd
@@ -7,17 +7,6 @@ onready var leftHalf = $LeftHalfBody
 onready var rightHalf = $RightHalfBody
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-
-func _process(delta):
-	
-	#velocity += gravity * delta
-	#$LeftHalfBody.sleeping = true
-	pass
-
 func _on_StaticCollisionTimer_timeout():
 	$LeftHalfBody.mode = RigidBody2D.MODE_STATIC
 	$RightHalfBody.mode =  RigidBody2D.MODE_STATIC
